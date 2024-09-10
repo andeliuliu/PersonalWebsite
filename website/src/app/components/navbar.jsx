58d424d5dd3132e60cd5585f182018daa543a8d7
andeliuliu <andelerliutv@gmail.com>
2024-09-09 22:56:31 -0400

    added pages

---
+++ website/src/app/components/navbar.jsx
@@ -0,0 +1,37 @@
+export default function Navbar() {
+    return (
+      <div className="fixed top-0 left-0 w-full z-50">
+        <nav className="bg-black text-white shadow-md">
+          <div className="container mx-auto p-4 flex justify-between items-center">
+            <div className="text-xl font-bold">
+                <a href="/" className="hover:text-gray-400">
+                  Home
+                </a>
+            </div>
+            <ul className="hidden md:flex space-x-6">
+              <li>
+                <a href="/" className="hover:text-gray-400">
+                  About Me
+                </a>
+              </li>
+              <li>
+                <a href="experience" className="hover:text-gray-400">
+                  Work Experience
+                </a>
+              </li>
+              <li>
+                <a href="projects" className="hover:text-gray-400">
+                  Projects
+                </a>
+              </li>
+              <li>
+                <a href="contact" className="hover:text-gray-400">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    )
+}
---
+++ website/src/app/contact/page.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/navbar"
+
+export default function Contact() {
+    return (
+        <div>
+            <Navbar />
+            <div className="pt-16">
+                Contact
+            </div>
+        </div>
+    )
+}
